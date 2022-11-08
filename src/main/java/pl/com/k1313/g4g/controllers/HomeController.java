93ed83f9440245926a0b1c81204707d0b6160e0c
kt1313 <69506082+kt1313@users.noreply.github.com>
2022-11-08 05:25:08 +0100

    1.0.4 - Home/Registry/Login - dodano te trzy templatki. Nie ma obslugi maila jeszcze
    TODO: obsluga eventu email i potem haslo z uzytkownikiem

---
+++ src/main/java/pl/com/k1313/g4g/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package pl.com.k1313.g4g.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login";
+    }
+
+}
